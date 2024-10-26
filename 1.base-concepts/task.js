35847dd1f084bf9a98429f04178ec4c4bf71342f
pavp5 <pavp5@yandex.ru>
2024-10-26 15:06:30 +0300

    Задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr = [];  
+  let D = b*b - 4*a*c;
+  if (D > 0) {
+    D =  Math.sqrt(D);
+    arr[0] = (-b + D)/(2*a);
+    arr[1] = (-b - D)/(2*a);
+  } else if (D == 0) {
+    arr[0] = -b/(2*a);
+  }
   return arr;
 }
 
