f08f537f61860d5b05543c3530324d0964954e1c
pavp5 <pavp5@yandex.ru>
2024-11-04 16:47:29 +0300

    Лекция 4. Задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks && this.marks.length > 0) {
+    return this.marks.reduce((sum, item) => sum += item, 0) / this.marks.length;
+  }
+  return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
 }
